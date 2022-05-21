85abe774fa6a8cbeee685343a457f573e14c09ec
Allani Ahmed <allania7med11@gmail.com>
2022-05-21 19:08:59 +0100

    SHP-15__add logo

---
+++ components/logo.js
@@ -0,0 +1,7 @@
+import { SvgIcon } from '@mui/material';
+import LogoIcon from 'public/Logo.svg';
+
+const Logo = () => (
+    <SvgIcon component={LogoIcon} viewBox="0 0 160 40" sx={{ fontSize: 150 }} />
+)
+export default Logo 
--- components/navbar.js
+++ components/navbar.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+import { AppBar, Box, Button, IconButton, Toolbar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import Logo from "components/logo.js"
+
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ bgcolor: "white", color:"black" }}>
+      <AppBar position="static" sx={{ bgcolor: "white", color: "black", height: "64px" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -21,9 +18,9 @@ export default function ButtonAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
+          <Button variant="text">
+            <Logo />
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
