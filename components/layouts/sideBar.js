d83b1f9b44972759627f99ea6ccaaa0416c65fa7
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-05-28 11:57:37 +0100

    SHP-15__fix  absolute-imports issue

--- components/layouts/sideBar.js
+++ components/layouts/sideBar.js
@@ -27,7 +27,6 @@ export default function MiniDrawer({ open, handleDrawerClose }) {
           boxSizing: 'border-box',
         },
       }}
-        variant=""
         anchor="left"
         open={open}>
         <DrawerHeader>
