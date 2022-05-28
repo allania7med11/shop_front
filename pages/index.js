e6fce74733bbfd68f396ec2021ffe1f8ecb9cade
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-05-28 14:07:59 +0100

    SHP-16__add PageTitle

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import ProTip from 'src/ProTip';
-import Link from 'src/Link';
 import Copyright from 'src/Copyright';
+import Box from '@mui/material/Box';
+import { PageTitle } from 'components/pages/pageTitle';
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <PageTitle>
+          Discover Our Products and Categories
+        </PageTitle>
         <ProTip />
         <Copyright />
       </Box>
