a2b2805694f3c81779e51f8d30fbf52e3ae6695c
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-12-26 15:38:28 +0100

    SHP-28__remove console.log

--- hooks/api/index.ts
+++ hooks/api/index.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("BACKEND_URL: ", process.env.NEXT_PUBLIC_BACKEND_URL)
 export const useApi = () => {
   const client = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
