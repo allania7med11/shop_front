29e1a7577b191a5c082258b5555550c79b1c59e9
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-12-26 15:26:01 +0100

    SHP-29__case slug is null

--- pages/categories/[slug].tsx
+++ pages/categories/[slug].tsx
@@ -1,26 +1,28 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Copyright from 'src/Copyright';
-import Box from '@mui/material/Box';
-import { PageTitle } from '@/components/common/pageTitle';
-import { useRouter } from 'next/router'
-import { useCategoryRead } from '@/hooks/api/categories';
-import { Category } from '@/components/pages/index/category';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Copyright from "src/Copyright";
+import Box from "@mui/material/Box";
+import { PageTitle } from "@/components/common/pageTitle";
+import { useRouter } from "next/router";
+import { useCategoryRead } from "@/hooks/api/categories";
+import { Category } from "@/components/pages/index/category";
 
 export default function Index() {
-    const router = useRouter()
-    const { slug } = router.query
+  const router = useRouter();
+  const { slug } = router.query;
+  let category = null;
+  if (slug) {
     const { data } = useCategoryRead(slug);
-    const category = data
-    return (
-        <Container maxWidth="lg">
-            <Box sx={{ my: 4 }}>
-                <PageTitle sx={{ py: 7 }}>
-                    category page
-                </PageTitle>
-                {category && <Category category={category} />}
-                <Copyright sx={{ py: 7 }} />
-            </Box>
-        </Container>
-    );
+    category = data;
+  }
+
+  return (
+    <Container maxWidth="lg">
+      <Box sx={{ my: 4 }}>
+        <PageTitle sx={{ py: 7 }}>category page</PageTitle>
+        {category && <Category category={category} />}
+        <Copyright sx={{ py: 7 }} />
+      </Box>
+    </Container>
+  );
 }
