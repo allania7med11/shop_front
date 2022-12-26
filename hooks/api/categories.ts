448e338c208fee0be6b96e5d6980c1bbc29f0f4e
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2022-12-26 17:34:46 +0100

    SHP-28__add enabled to useQuery and fix queryKey

--- hooks/api/categories.ts
+++ hooks/api/categories.ts
@@ -3,22 +3,22 @@ import { useQuery } from "react-query";
 import { IsCategory } from "../../data/categories";
 
 
-export const useCategoriesRead = () => {
+export const useCategoriesRead = (enabled=true) => {
   const client = useApi();
   const queryFn = async () => {
     const reponse = await client.get("/categories/");
     const categories: IsCategory[]  = reponse.data;
     return categories
   };
-  return useQuery("categories", queryFn);
+  return useQuery("categories", queryFn, {enabled: enabled});
 };
 
-export const useCategoryRead = (slug) => {
+export const useCategoryRead = (slug, enabled=true) => {
   const client = useApi();
   const queryFn = async () => {
     const reponse = await client.get(`/categories/${slug}/`);
     const category: IsCategory = reponse.data;
     return category
   };
-  return useQuery("categories", queryFn);
+  return useQuery(["category", slug], queryFn, {enabled: enabled});
 };
