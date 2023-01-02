153325da19bef2ec566d4c3d6d0a4f3d65a4ae15
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-02 21:33:10 +0100

    SHP-28__layout products

--- components/pages/categories/products.tsx
+++ components/pages/categories/products.tsx
@@ -1,14 +1,22 @@
 import "swiper/css";
 import "swiper/css/navigation";
-import { blueGrey } from "@mui/material/colors";
 import Box from "@mui/material/Box";
 import { IsProduct } from "@/data/categories";
 import { FC } from "react";
 import { CardProduct } from "@/components/common/cardProduct";
+import { SxProps } from "@mui/material";
+
+const sxProducts: SxProps = {
+  p: "24px",
+  display: "grid",
+  gap: "24px",
+  gridTemplateColumns: "repeat(auto-fill, minmax(min(230px, 100%), 1fr))",
+  justifyItems: "center"
+};
 
 export const Products: FC<{ products: IsProduct[] }> = ({ products }) => {
   return (
-    <Box sx={{ p: "24px" }}>
+    <Box sx={sxProducts}>
       {products &&
         products.map((product, key) => (
           <CardProduct key={key} product={product} />
