e9b3fd23bb8eccc1483f06620c80cf0646b5755b
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-02 21:32:44 +0100

    SHP-28__add search icon

--- components/pages/categories/filters/search.tsx
+++ components/pages/categories/filters/search.tsx
@@ -1,51 +1,61 @@
 import { IsProduct } from "@/data/categories";
-import { Autocomplete, Box, Popper, TextField } from "@mui/material";
+import { Autocomplete, Box, InputAdornment, Popper, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import React, { FC } from "react";
 
-
-
-
-export const Search: FC<{ products: IsProduct[] }> = ({products}) => {
-    const CustomPopper = React.useCallback((props) => {
-        return (
-          <Popper
-            {...props}
-            placement="bottom-start"
-            style={{
-              width: "300px",
-            }}
-          />
-        );
-      }, []);
+export const Search: FC<{ products: IsProduct[] }> = ({ products }) => {
+  const CustomPopper = React.useCallback((props) => {
     return (
-        <Autocomplete
-          PopperComponent={CustomPopper}
-          sx={{
-            root: {
-              "& ul": {
-                backgroundColor: "green",
-              },
-            },
+      <Popper
+        {...props}
+        placement="bottom-start"
+        style={{
+          width: "300px",
+        }}
+      />
+    );
+  }, []);
+  return (
+    <Autocomplete
+      PopperComponent={CustomPopper}
+      sx={{
+        root: {
+          "& ul": {
+            backgroundColor: "green",
+          },
+        },
+      }}
+      id="free-solo-demo"
+      freeSolo
+      options={products}
+      getOptionLabel={(option: IsProduct) => option.name}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Search"
+          sx={{ backgroundColor: "white" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
           }}
-          id="free-solo-demo"
-          freeSolo
-          options={products}
-          getOptionLabel={(option: IsProduct) => option.name}
-          renderInput={(params) => <TextField {...params} label="Search" />}
-          renderOption={(props, option: IsProduct) => (
-            <Box
-              component="li"
-              sx={{
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              }}
-              {...props}
-            >
-              {option.name}
-            </Box>
-          )}
         />
-
-    )
-}
+      )}
+      renderOption={(props, option: IsProduct) => (
+        <Box
+          component="li"
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+          {...props}
+        >
+          {option.name}
+        </Box>
+      )}
+    />
+  );
+};
