509e0f34beed9906c4b4cd7dfcff90e16af335bf
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-03 19:09:02 +0100

    SHP-28__fix Autocomplete not working

--- components/pages/categories/filters/search.tsx
+++ components/pages/categories/filters/search.tsx
@@ -1,48 +1,49 @@
 import { IsProduct } from "@/data/categories";
-import { Autocomplete, Box, InputAdornment, Popper, TextField } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import {
+  Autocomplete,
+  Box,
+  InputAdornment,
+  Popper,
+  TextField,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import React, { FC } from "react";
 
+const CustomPopper = (props) => {
+  return (
+    <Popper
+      {...props}
+      placement="bottom-start"
+      style={{
+        width: "300px",
+      }}
+    />
+  );
+};
+
+const renderInput = (params) => {
+  params.InputProps.startAdornment = (
+    <>
+      <InputAdornment position="start">
+        <SearchIcon />
+      </InputAdornment>
+      {params.InputProps.startAdornment}
+    </>
+  );
+  return (
+    <TextField {...params} label="Search" sx={{ backgroundColor: "white" }} />
+  );
+};
+
 export const Search: FC<{ products: IsProduct[] }> = ({ products }) => {
-  const CustomPopper = React.useCallback((props) => {
-    return (
-      <Popper
-        {...props}
-        placement="bottom-start"
-        style={{
-          width: "300px",
-        }}
-      />
-    );
-  }, []);
   return (
     <Autocomplete
       PopperComponent={CustomPopper}
-      sx={{
-        root: {
-          "& ul": {
-            backgroundColor: "green",
-          },
-        },
-      }}
       id="free-solo-demo"
       freeSolo
       options={products}
       getOptionLabel={(option: IsProduct) => option.name}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search"
-          sx={{ backgroundColor: "white" }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      )}
+      renderInput={renderInput}
       renderOption={(props, option: IsProduct) => (
         <Box
           component="li"
