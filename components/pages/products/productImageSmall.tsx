3aaaedd47a170fb867eb0e3db919c3ba4456e855
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-13 21:03:24 +0100

    SHP-30__add custom style to active file

--- components/pages/products/productImageSmall.tsx
+++ components/pages/products/productImageSmall.tsx
@@ -1,18 +1,30 @@
 import { FC } from "react";
 import { Box, CardMedia, Paper, SxProps } from "@mui/material";
 import { IsFile } from "@/data/categories";
+import { blue, grey } from "@mui/material/colors";
 
-const sxPaper: SxProps = {
+const sxPaperDefault: SxProps = {
   display: "flex",
   justifyContent: "center",
   width: "50px",
   height: "50px",
   padding: "8px",
+  border: `1px solid ${grey[400]}`
+};
+const sxPaperActive: SxProps = {
+  border: `1px solid ${blue[800]}`,
+  boxShadow: `0px 3px 3px -2px ${blue[800]}, 0px 3px 4px 0px ${blue[800]}, 0px 1px 8px 0px ${blue[800]}`,
 };
 
 export const ProductImageSmall: FC<{
   file: IsFile;
-}> = ({ file }) => {
+  index: number;
+  activeFileIndex: number;
+}> = ({ file, index, activeFileIndex }) => {
+  const sxPaper =
+    activeFileIndex == index
+      ? { ...sxPaperDefault, ...sxPaperActive }
+      : sxPaperDefault;
   return (
     <Box sx={{ display: "flex", justifyContent: "center", padding: "8px" }}>
       <Paper elevation={3} sx={sxPaper}>
--- components/pages/products/productImages.tsx
+++ components/pages/products/productImages.tsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Box } from "@mui/material";
 import { IsFile } from "@/data/categories";
 import { ProductImageSmall } from "./productImageSmall";
@@ -11,20 +11,21 @@ import { ProductImageBig } from "./productImageBig";
 export const ProductImages: FC<{
   files: IsFile[];
 }> = ({ files }) => {
+  const [activeFileIndex, setActiveFileIndex] = useState(0);
   return (
-    <Box sx={{ display: "flex", flexDirection:"column", gap: "24px" }}>
-      <ProductImageBig file={files[0]} />
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+      <ProductImageBig file={files[activeFileIndex]} />
       <Box sx={{ "& .swiper-slide": { width: "100%" } }}>
         <Swiper
           navigation={true}
           modules={[Navigation]}
           slidesPerView={3}
-          onSlideChange={() => console.log("slide change")}
+          onSlideChange={({ activeIndex }) => setActiveFileIndex(activeIndex)}
           onSwiper={(swiper) => console.log(swiper)}
         >
           {files.map((file, key) => (
             <SwiperSlide>
-              <ProductImageSmall key={key} file={file} />
+              <ProductImageSmall key={key} index={key} file={file} activeFileIndex={activeFileIndex} />
             </SwiperSlide>
           ))}
         </Swiper>
