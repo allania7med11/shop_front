414abb353350557a705676cf5b316cda170caa72
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-14 09:24:07 +0100

    SHP-30__change swiper-button color to black

--- components/pages/products/productImages.tsx
+++ components/pages/products/productImages.tsx
@@ -3,10 +3,15 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import { FC, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, SxProps } from "@mui/material";
 import { IsFile } from "@/data/categories";
 import { ProductImageSmall } from "./productImageSmall";
 import { ProductImageBig } from "./productImageBig";
+
+const sxSwiper: SxProps = {
+  "& .swiper-slide": { width: "100%" },
+  "& .swiper-button-prev, .swiper-button-next": { color: "black" },
+};
 
 export const ProductImages: FC<{
   files: IsFile[];
@@ -14,13 +19,13 @@ export const ProductImages: FC<{
   const [swiper, setSwiper] = useState(null);
   const [activeFileIndex, setActiveFileIndex] = useState(0);
   const updateActiveFileIndex: (index: number) => any = (index) => {
-    swiper.slideTo(index)
-    setActiveFileIndex(index)
+    swiper.slideTo(index);
+    setActiveFileIndex(index);
   };
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: "24px" }}>
       <ProductImageBig file={files[activeFileIndex]} />
-      <Box sx={{ "& .swiper-slide": { width: "100%" } }}>
+      <Box sx={sxSwiper}>
         <Swiper
           onSwiper={setSwiper}
           navigation={true}
@@ -30,7 +35,12 @@ export const ProductImages: FC<{
         >
           {files.map((file, key) => (
             <SwiperSlide key={key}>
-              <ProductImageSmall  index={key} file={file} activeFileIndex={activeFileIndex} updateActiveFileIndex={updateActiveFileIndex} />
+              <ProductImageSmall
+                index={key}
+                file={file}
+                activeFileIndex={activeFileIndex}
+                updateActiveFileIndex={updateActiveFileIndex}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
