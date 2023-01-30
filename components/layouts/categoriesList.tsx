56d688a0a1fc898e7bd9c0b126ab43ff04962575
ahmedallanisoftcatalyst <ahmed.allani@softcatalyst.com>
2023-01-30 22:13:22 +0100

    SHP-36__update Item style

--- components/layouts/categoriesList.tsx
+++ components/layouts/categoriesList.tsx
@@ -1,67 +1,57 @@
-import { Icon } from "@iconify/react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListSubheader from "@mui/material/ListSubheader";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { Link } from "../common/Link";
 import { grey } from "@mui/material/colors";
+import { useCategoriesQuery } from "@/store/reducer/apis/productApi";
+import { SxProps } from "@mui/material";
 
-const linkSX = {
-    "& a": {
-      color: grey[800],
-      display: "flex",
-      alignItems: "center",
-      "&:-webkit-any-link": {
-        textDecoration: "none",
-      },
-      "&:hover": {
-        textDecoration: "none",
-      },
+const sxItem: SxProps = {
+  display: "flex",
+  "& >div": {
+    flexGrow: 1,
+  },
+  "& a": {
+    with: "100%",
+    color: grey[700],
+    display: "flex",
+    alignItems: "center",
+    "&:-webkit-any-link": {
+      textDecoration: "none",
     },
-  };
+    "&:hover": {
+      textDecoration: "none",
+      backgroundColor: grey[100],
+      color: grey[900],
+    },
+  },
+};
 
-const categories = [
-  {
-    name: "Mobiles",
-    slug: "mobiles",
-    icon: "entypo:mobile",
-  },
-  {
-    name: "Laptops",
-    slug: "laptops",
-    icon: "ic:round-laptop",
-  },
-  {
-    name: "Tvs",
-    slug: "tvs",
-    icon: "ic:baseline-tv",
-  },
-];
-
-export const CategoriesList = () => (
-  <List
-    sx={{
-      overflow: "auto",
-      maxHeight: "90%",
-      "& ul": { padding: 0 },
-    }}
-    subheader={<li />}
-  >
-    <li key={`section-categories`}>
-      <ul>
-        <ListSubheader>{`CATEGORIES`}</ListSubheader>
-        {categories.map((category) => (
-          <ListItem key={`item-category-${category.name}`}>
-            <Link href={`/categories/${category.slug}/`} sx={linkSX}>
-              <ListItemIcon sx={{ minWidth: 48, pl: 2.5 }}>
-                <Icon icon={`${category.icon}`} />
-              </ListItemIcon>
-              <ListItemText primary={`${category.name}`} />
-            </Link>
-          </ListItem>
-        ))}
-      </ul>
-    </li>
-  </List>
-);
+export const CategoriesList = () => {
+  const { data } = useCategoriesQuery();
+  return (
+    <List
+      sx={{
+        overflow: "auto",
+        maxHeight: "90%",
+        "& ul": { padding: 0 },
+      }}
+      subheader={<li />}
+    >
+      <li key={`section-categories`}>
+        <ul>
+          <ListSubheader>{`CATEGORIES`}</ListSubheader>
+          {data &&
+            data.map((category) => (
+              <ListItem key={`item-category-${category.name}`} sx={sxItem}>
+                <Link href={`/categories/${category.slug}/`} >
+                  <ListItemText sx={{  pl: 4 }} primary={`${category.name}`} />
+                </Link>
+              </ListItem>
+            ))}
+        </ul>
+      </li>
+    </List>
+  );
+};
