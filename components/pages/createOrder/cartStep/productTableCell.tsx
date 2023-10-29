305b1aff4318a13baa2b67498db508b711ee3f8c
allania7med11 <allania7med11@gmail.com>
2023-10-29 13:11:53 +0000

    add remove button to ProductTableCell

--- components/pages/createOrder/cartStep/productTableCell.tsx
+++ components/pages/createOrder/cartStep/productTableCell.tsx
@@ -2,14 +2,17 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
 import { IsProduct } from "@/data/categories";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { Link } from "@/components/common/Link";
+import { useDeleteCartItemMutation } from "@/store/reducer/apis/cartApi";
 
 export const ProductTableCell: React.FC<{ product: IsProduct }> = ({
   product,
 }) => {
   const { name, files } = product;
   const imageUrl = files.length > 0 ? files[0].url : "";
+  const [deleteItem, {}] =
+    useDeleteCartItemMutation();
   return (
     <Box sx={{ display: "flex", px: 1, alignItems: "start" }}>
       <Link href={`/products?slug=${product.slug}`}>
@@ -21,12 +24,15 @@ export const ProductTableCell: React.FC<{ product: IsProduct }> = ({
           sx={{ objectFit: "contain" }}
         ></Box>
       </Link>
-      <Box sx={{ padding: "0px 24px"}}>
+      <Box sx={{ padding: "0px 24px" }}>
         <Tooltip title={name} arrow>
-          <Typography noWrap gutterBottom variant="h6" component="div">
+          <Typography noWrap gutterBottom variant="h6" component="div" sx={{ mb: 0 }}>
             {name}
           </Typography>
         </Tooltip>
+        <Button variant="text" color="error" sx={{ textTransform: "none" }} onClick={() => deleteItem(product.cart_item.id)}>
+          Remove
+        </Button>
       </Box>
     </Box>
   );
