7c67cf0e78aefbb782807b04efec6199e29bca9a
allania7med11 <allania7med11@gmail.com>
2023-11-16 22:46:44 +0000

    865d8uh5w__add pagination

--- components/pages/createOrder/cartStep/cartTable.tsx
+++ components/pages/createOrder/cartStep/cartTable.tsx
@@ -6,6 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
 import { IsProduct } from "@/data/categories";
 import { ProductQuantity } from "@/components/common/productQuantity";
 import { ProductTableCell } from "./productTableCell";
@@ -37,27 +38,53 @@ export const CartTable = () => {
   const { data: productsApi = [] } = useProductsQuery(
     { id_in: product_ids },
     {
-      skip: product_ids.length == 0,
+      skip: product_ids.length === 0,
     }
   );
   const products = addItemsToProducts(productsApi, items);
 
+  // Pagination state
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5); // Set the number of rows per page
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table" sx={{ "& td": { padding: "12px 4px" } }}>
-        <TableHead>
-          <TableRow>
-            {headers.map((header, key) => (
-              <TableCell key={key}>{header}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product, key) => (
-            <CartTableRow key={key} product={product} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Paper>
+      <TableContainer>
+        <Table aria-label="simple table" sx={{ "& td": { padding: "12px 4px" } }}>
+          <TableHead>
+            <TableRow>
+              {headers.map((header, key) => (
+                <TableCell key={key}>{header}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((product, key) => (
+                <CartTableRow key={key} product={product} />
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={products.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
