5438689540f92d12dcae5b0e3afa367b7edbbb54
allania7med11 <allania7med11@gmail.com>
2023-12-17 13:03:12 +0000

    862knavgx__add login request

--- components/common/login.tsx
+++ components/common/login.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { InputLabel, Paper, Typography } from '@mui/material';
+import { CircularProgress, InputLabel, Paper, Typography } from '@mui/material';
 import LogoSmall from './logoSmall';
 import { useForm } from 'react-hook-form';
 import FormTextField from './Form/formTextField';
+import { useLoginMutation } from '@/store/reducer/apis/authApi';
 
 export const Login = () => {
   const { control, handleSubmit } = useForm();
+  const [login, response] = useLoginMutation(); // Destructure the login mutation and loading state
+  const { isLoading, error, data } = response
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (form_data) => {
+    try {
+      // Trigger the login mutation with the form data
+      await login(form_data);
+
+      // Check if there are any errors in the response
+      if (error) {
+        console.error('Login failed', error);
+      } else {
+        // Access the successful response data
+        console.log('Login successful!', data);
+      }
+
+    } catch (error) {
+      console.error('Login failed', error);
+    }
   };
 
   return (
@@ -21,44 +38,46 @@ export const Login = () => {
       "gap": "16px",
       "width": "min(100%, 500px)"
     }}>
-      <Box sx={{display: "flex", flexDirection: "column", gap: 1, alignItems: "center", pb: 2}}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1, alignItems: "center", pb: 2 }}>
         <LogoSmall />
         <Typography variant="h6">
           Welcome To Logipsum
         </Typography>
       </Box>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Box>
-          <InputLabel htmlFor="email">Email</InputLabel>
-          <FormTextField
-            name="email"
-            control={control}
-            defaultValue=""
-            rules={{ required: 'Email is required' }}
-            type="email"
-            id="email"
-            placeholder="example@gmail.com"
-            variant="outlined"
-            fullWidth
-          />
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Box>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <FormTextField
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Email is required' }}
+              type="email"
+              id="email"
+              placeholder="example@gmail.com"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
+          <Box>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <FormTextField
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'Password is required' }}
+              type="password"
+              id="password"
+              placeholder="********"
+              variant="outlined"
+              fullWidth
+            />
+          </Box>
         </Box>
-        <Box>
-          <InputLabel htmlFor="password">Password</InputLabel>
-          <FormTextField
-            name="password"
-            control={control}
-            defaultValue=""
-            rules={{ required: 'Password is required' }}
-            type="password"
-            id="password"
-            placeholder="********"
-            variant="outlined"
-            fullWidth
-          />
-        </Box>
-        <Box sx={{display: "flex", justifyContent: "center", pt:2}}>
-          <Button type="submit" sx={{ textTransform: "none" }} size="large" variant="contained" color="primary">
-            Login
+        <Box sx={{ display: "flex", justifyContent: "center", pt: 4 }}>
+          <Button type="submit" variant="contained" color="primary" disabled={isLoading}>
+            {isLoading ? <CircularProgress size={24} color="inherit" /> : 'Login'}
           </Button>
         </Box>
       </form>
