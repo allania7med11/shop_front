cdb618c98d51535f9db8cea68ed5766886683ddd
allania7med11 <allania7med11@gmail.com>
2023-12-24 10:32:47 +0000

    862kpe6p4__add X-CSRFToken to login

--- store/reducer/apis/authApi.ts
+++ store/reducer/apis/authApi.ts
@@ -1,12 +1,19 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { baseUrl } from "./utils";
 import { AuthCredentials, IsUser, LoginCredentials } from "@/data/auth";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
     baseUrl: baseUrl,
-    credentials: "include"
+    credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      headers.set("X-CSRFToken", cookies.get("csrftoken"))
+      return headers
+    }
   }),
   tagTypes: ["User"],
   endpoints: (builder) => ({
