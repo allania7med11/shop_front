d7e9eab435ac7fefcaf8449a6a1cb5e860f453a1
allania7med11 <allania7med11@gmail.com>
2023-12-28 22:52:00 +0000

    862kpe6p4__protect anonymous urls

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -3,8 +3,10 @@ import Container from "@mui/material/Container";
 import Copyright from "src/Copyright";
 import Box from "@mui/material/Box";
 import { Login } from "@/components/common/login";
+import useRedirectIfLoggedIn from "@/hooks/useRedirectIfLoggedIn";
 
 export default function Index() {
+  useRedirectIfLoggedIn()
   return (
     <Container maxWidth={false} sx={{ maxWidth: "1400px" }}>
       <Box sx={{ my: 4 }}>
--- pages/auth/register/index.tsx
+++ pages/auth/register/index.tsx
@@ -3,8 +3,10 @@ import Container from "@mui/material/Container";
 import Copyright from "src/Copyright";
 import Box from "@mui/material/Box";
 import { Register } from "@/components/common/register";
+import useRedirectIfLoggedIn from "@/hooks/useRedirectIfLoggedIn";
 
 export default function Index() {
+  useRedirectIfLoggedIn()
   return (
     <Container maxWidth={false} sx={{ maxWidth: "1400px" }}>
       <Box sx={{ my: 4 }}>
