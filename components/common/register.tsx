85d20efea98726ca65a4fc71259ba8991044c2cc
allania7med11 <allania7med11@gmail.com>
2023-12-29 14:25:30 +0000

    862kpe6p4__use FormPasswordField in Register

--- components/common/register.tsx
+++ components/common/register.tsx
@@ -13,11 +13,16 @@ import { useForm } from "react-hook-form";
 import FormTextField from "./Form/formTextField";
 import { useRegisterMutation } from "@/store/reducer/apis/authApi";
 import useErrors from "@/hooks/useErrors";
+import FormPasswordField from "./Form/formPasswordField";
 
 export const Register = () => {
   const { control, handleSubmit, setError, clearErrors, getValues } = useForm();
   const [register, { isLoading, error, isSuccess }] = useRegisterMutation();
-  const { globalErrors, setGlobalErrors } = useErrors(error, setError, getValues);
+  const { globalErrors, setGlobalErrors } = useErrors(
+    error,
+    setError,
+    getValues
+  );
   const onSubmit = async (form_data) => {
     clearErrors();
     setGlobalErrors([]);
@@ -52,7 +57,11 @@ export const Register = () => {
           sx={{ display: "flex", flexDirection: "column", gap: 1, my: "16px" }}
         >
           {globalErrors &&
-            globalErrors.map((error, key) => <Alert key={key} severity="error">{error}</Alert>)}
+            globalErrors.map((error, key) => (
+              <Alert key={key} severity="error">
+                {error}
+              </Alert>
+            ))}
         </Box>
         <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
           <Box sx={{ display: "flex", gap: 2 }}>
@@ -99,12 +108,11 @@ export const Register = () => {
           </Box>
           <Box>
             <InputLabel htmlFor="password">Password</InputLabel>
-            <FormTextField
+            <FormPasswordField
               name="password1"
               control={control}
               defaultValue=""
               rules={{ required: "Password is required" }}
-              type="password"
               id="password"
               placeholder="********"
               variant="outlined"
@@ -113,12 +121,11 @@ export const Register = () => {
           </Box>
           <Box>
             <InputLabel htmlFor="password">Retype Password</InputLabel>
-            <FormTextField
+            <FormPasswordField
               name="password2"
               control={control}
               defaultValue=""
               rules={{ required: "Retype Password is required" }}
-              type="password"
               id="retype_password"
               placeholder="********"
               variant="outlined"
@@ -127,7 +134,12 @@ export const Register = () => {
           </Box>
         </Box>
         <Box sx={{ display: "flex", justifyContent: "center", pt: 4 }}>
-          <Button type="submit" variant="contained" color="primary" disabled={isLoading || isSuccess}>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={isLoading || isSuccess}
+          >
             {isLoading || isSuccess ? (
               <CircularProgress size={24} color="inherit" />
             ) : (
