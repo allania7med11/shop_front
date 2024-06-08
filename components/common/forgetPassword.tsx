673e116b2e7073aa2aec8763c4bc0c7cce2f5d94
allania7med11 <allania7med11@gmail.com>
2024-06-08 10:49:12 +0000

    SHP-2405242__add SuccessMessage to ForgetPassword

--- components/common/forgetPassword.tsx
+++ components/common/forgetPassword.tsx
@@ -10,10 +10,11 @@ import useErrors from "@/hooks/useErrors";
 import { sxAuthButton } from "@/styles/authButtonStyle";
 import SendEmailIcon from "./Icons/sendEmail";
 import { grey } from "@mui/material/colors";
+import { SuccessMessage } from "./successMessage";
 
 export const ForgetPassword = () => {
   const { control, handleSubmit, setError, clearErrors, getValues } = useForm();
-  const [resetPassword, { isLoading, error, isSuccess }] = useResetPasswordMutation(); 
+  const [resetPassword, { isLoading, error, isSuccess }] = useResetPasswordMutation();
   const { globalErrors, setGlobalErrors } = useErrors(
     error,
     setError,
@@ -36,57 +37,61 @@ export const ForgetPassword = () => {
         width: "min(100%, 500px)",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          pb: 2,
-        }}
-      >
-        <LogoSmall />
-        <Typography variant="h6">Forgot Account Password</Typography>
-      </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
+
+      {!isSuccess && (
         <Box
-          sx={{ display: "flex", flexDirection: "column", gap: 1, my: "0px" }}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            pb: 2,
+          }}
         >
-          {globalErrors &&
-            globalErrors.map((error, key) => (
-              <Alert key={key} severity="error">
-                {error}
-              </Alert>
-            ))}
+          <LogoSmall />
+          <Typography variant="h6">Forgot Account Password</Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box
+              sx={{ display: "flex", flexDirection: "column", gap: 1, my: "0px" }}
+            >
+              {globalErrors &&
+                globalErrors.map((error, key) => (
+                  <Alert key={key} severity="error">
+                    {error}
+                  </Alert>
+                ))}
+            </Box>
+            <Typography sx={{ px: 1, py: 2 }} color={grey[700]}>
+              Enter the email address associated with your account and we will send you a link to reset your password.
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+              <Box>
+                <InputLabel htmlFor="email">Email</InputLabel>
+                <FormTextField
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: "Email is required" }}
+                  type="email"
+                  id="email"
+                  placeholder="example@gmail.com"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Box>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center", pt: 4 }}>
+              <Button type="submit" variant="contained" color="primary" disabled={isLoading || isSuccess} sx={sxAuthButton} endIcon={<SendEmailIcon />}>
+                {isLoading || isSuccess ? (
+                  <CircularProgress size={24} color="inherit" />
+                ) : (
+                  "Send"
+                )}
+              </Button>
+            </Box>
+          </form>
         </Box>
-        <Typography sx={{ px: 1, py:2 }} color={grey[700]}>
-            Enter the email address associated with your account and we will send you a link to reset your password.
-        </Typography>
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-          <Box>
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <FormTextField
-              name="email"
-              control={control}
-              defaultValue=""
-              rules={{ required: "Email is required" }}
-              type="email"
-              id="email"
-              placeholder="example@gmail.com"
-              variant="outlined"
-              fullWidth
-            />
-          </Box>
-        </Box>
-        <Box sx={{ display: "flex", justifyContent: "center", pt: 4 }}>
-          <Button type="submit" variant="contained" color="primary" disabled={isLoading} sx={sxAuthButton} endIcon={<SendEmailIcon />}>
-            {isLoading ? (
-              <CircularProgress size={24} color="inherit" />
-            ) : (
-              "Send"
-            )}
-          </Button>
-        </Box>
-      </form>
+      )}
+      {isSuccess && <SuccessMessage message="Your password reset link was emailed successfully" />}
     </Paper>
   );
 };
