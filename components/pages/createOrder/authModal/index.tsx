cafc329454f765a122f4e28a4a8c9261ccfb4f5d
allania7med11 <allania7med11@gmail.com>
2024-06-15 15:59:47 +0000

    SHP-2405243__add Divider

--- components/pages/createOrder/authModal/index.tsx
+++ components/pages/createOrder/authModal/index.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/components/pages/createOrder/authModal/loginForm";
-import { Box, Card, Modal } from "@mui/material"
+import { Box, Card, Divider, Fab, Modal } from "@mui/material"
 import { RegisterForm } from "./registerForm";
+import { grey } from "@mui/material/colors";
 
 export const AuthModal = ({ open, onClose }) => {
     return (
@@ -18,17 +19,30 @@ export const AuthModal = ({ open, onClose }) => {
                     transform: "translate(-50%, -50%)",
                     display: "flex",
                     justifyContent: "space-evenly",
-                    padding: "32px 48px",
+                    padding: "32px 48px 72px 48px",
                     gap: "64px",
                     width: "90%",
                     maxWidth: "1200px",
                     outline: "none"
                 }}
             >
-                <Box sx={{ width: "40%", maxWidth: "400px" }}>
+                <Box sx={{ width: "40%", maxWidth: "400px", pt: "20px" }}>
                     <RegisterForm />
                 </Box>
-                <Box sx={{ width: "40%", maxWidth: "400px" }}>
+                <Box sx={{ backgroundColor: grey[300], width: "2px" }}>
+                    <Fab
+                        sx={{
+                            position: "absolute",
+                            top: "25%",
+                            left: "50%",
+                            transform: "translate(-50%, -25%)",
+                            backgroundColor: grey[200],
+                            color: grey[700],
+                        }}>
+                        OR
+                    </Fab>
+                </Box>
+                <Box sx={{ width: "40%", maxWidth: "400px", pt: "20px" }}>
                     <LoginForm />
                 </Box>
             </Card>
