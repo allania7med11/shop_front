c7282c228d2e07e073d65545e2a986ed4dc4f2a0
allania7med11 <allania7med11@gmail.com>
2024-06-17 10:43:02 +0000

    SHP-2405243__make AuthModal responsive

--- components/pages/createOrder/authModal/index.tsx
+++ components/pages/createOrder/authModal/index.tsx
@@ -1,64 +1,82 @@
 import { LoginForm } from "@/components/pages/createOrder/authModal/loginForm";
-import { Box, Card, Fab, IconButton, Modal } from "@mui/material"
+import { Box, Card, Fab, IconButton, Modal, SxProps } from "@mui/material";
 import { RegisterForm } from "./registerForm";
 import { grey } from "@mui/material/colors";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 
-export const AuthModal = ({ open, onClose }) => {
-    return (
-        <Modal
-            open={open}
-            onClose={onClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-        >
-            <Card
-                sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    display: "flex",
-                    justifyContent: "space-evenly",
-                    padding: "32px 48px 72px 48px",
-                    gap: "64px",
-                    width: "90%",
-                    maxWidth: "1200px",
-                    outline: "none"
-                }}
-            >
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-                <Box sx={{ width: "40%", maxWidth: "400px", pt: "20px" }}>
-                    <RegisterForm />
-                </Box>
-                <Box sx={{ backgroundColor: grey[300], width: "2px" }}>
-                    <Fab
-                        sx={{
-                            position: "absolute",
-                            top: "25%",
-                            left: "50%",
-                            transform: "translate(-50%, -25%)",
-                            backgroundColor: grey[200],
-                            color: grey[700],
-                        }}>
-                        OR
-                    </Fab>
-                </Box>
-                <Box sx={{ width: "40%", maxWidth: "400px", pt: "20px" }}>
-                    <LoginForm />
-                </Box>
-            </Card>
-        </Modal>
-    )
-}
+const cardStyles: SxProps = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  display: "flex",
+  alignItems: { xs: "center", md: "stretch" },
+  justifyContent: "space-evenly",
+  padding: "32px 48px 72px 48px",
+  gap: "64px",
+  width: "90%",
+  maxWidth: { xs: "600px", md: "1200px" },
+  outline: "none",
+  flexDirection: { xs: "column", md: "row" },
+  maxHeight: "95vh",
+  overflowY: "auto",
+};
+
+const closeButtonStyles: SxProps = {
+  position: "absolute",
+  right: 8,
+  top: 8,
+  color: grey[500],
+};
+
+const boxStyles: SxProps = {
+  width: { xs: "90%", md: "40%" },
+  maxWidth: "400px",
+  pt: "20px",
+  flexGrow: 1,
+};
+
+const separatorBoxStyles: SxProps = {
+  backgroundColor: grey[300],
+  width: { xs: "100%", md: "2px" },
+  minHeight: { xs: "2px", md: "100%" },
+  position: "relative",
+};
+
+const fabStyles: SxProps = {
+  position: "absolute",
+  top: { xs: "50%", md: "25%" },
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  "@media (min-width:900px)": {
+    transform: "translate(-50%, -25%)", // Override transform for md and up
+  },
+  backgroundColor: grey[200],
+  color: grey[700],
+};
+
+export const AuthModal = ({ open, onClose }: { open: boolean, onClose: () => void }) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+    >
+      <Card sx={cardStyles}>
+        <IconButton aria-label="close" onClick={onClose} sx={closeButtonStyles}>
+          <CloseIcon />
+        </IconButton>
+        <Box sx={boxStyles}>
+          <RegisterForm />
+        </Box>
+        <Box sx={separatorBoxStyles}>
+          <Fab sx={fabStyles}>
+            OR
+          </Fab>
+        </Box>
+        <Box sx={boxStyles}>
+          <LoginForm />
+        </Box>
+      </Card>
+    </Modal>
+  );
+};
