8f7b0f3016058d40801253914735e237f88682ff
allania7med11 <allania7med11@gmail.com>
2024-06-19 11:41:38 +0000

    SHP-2405245__add OrderCompleteStep

--- components/pages/createOrder/orderCompleteStep/index.tsx
+++ components/pages/createOrder/orderCompleteStep/index.tsx
@@ -1,1 +1,34 @@
-export const OrderCompleteStep = () => "Order Complete Step"
+import React from "react";
+import { Typography, Paper } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+export const OrderCompleteStep = () => (
+  <Paper
+    elevation={3}
+    sx={{
+      display: "flex",
+      padding: "32px 48px",
+      flexDirection: "column",
+      width: "100%",
+      margin: "auto",
+      alignItems: "center",
+      justifyContent: "center",
+      textAlign: "center",
+    }}
+  >
+    <CheckCircleIcon
+      sx={{ fontSize: 60, marginBottom: "20px", color: "success.light" }}
+    />
+    <Typography
+      variant="h5"
+      component="h1"
+      gutterBottom
+      sx={{ color: "success.light", fontWeight: "bold" }}
+    >
+      Congratulations! Your order has been placed
+    </Typography>
+    <Typography variant="body1" color="textSecondary">
+      We will contact you by phone to confirm your order details.
+    </Typography>
+  </Paper>
+);
