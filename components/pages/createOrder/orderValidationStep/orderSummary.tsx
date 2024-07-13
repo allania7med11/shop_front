f1e7f61bb8ca9b65e26cf404ecacfd07d6f2e6cc
allania7med11 <allania7med11@gmail.com>
2024-07-13 14:46:22 +0000

    SHP-2406301__complete OrderSummary static

--- components/pages/createOrder/orderValidationStep/orderSummary.tsx
+++ components/pages/createOrder/orderValidationStep/orderSummary.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import {
     Box,
+    Divider,
     Paper,
     Typography
 } from "@mui/material";
+
+const products = [
+    { name: "HP Pavilion 15-DK1056WM", quantity: 2, price: 2062.16 },
+    { name: "Samsung Universe 9", quantity: 1, price: 1055.9 },
+    { name: "iPhone 9", quantity: 3, price: 1433.55 }
+];
 
 const OrderSummary = () => {
     return (
@@ -13,19 +20,16 @@ const OrderSummary = () => {
                     Order Summary
                 </Typography>
             </Box>
-            <Box sx={{ padding: "16px 48px 32px" }}>
-                <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
-                    <Typography>HP Pavilion 15-DK1056WM x 2</Typography>
-                    <Typography>$2062.16</Typography>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
-                    <Typography>Samsung Universe 9 x 1</Typography>
-                    <Typography>$1055.9</Typography>
-                </Box>
-                <Box sx={{ display: "flex", justifyContent: "space-between", marginBottom: "8px" }}>
-                    <Typography>iPhone 9 x 3</Typography>
-                    <Typography>$1433.55</Typography>
-                </Box>
+            <Box sx={{ padding: "16px 48px 32px", display: "flex", flexDirection: "column", gap: 1 }}>
+                {products.map((product, index) => (
+                    <Box key={index} sx={{ display: "flex", flexDirection: "column", gap: 0.5 }}>
+                        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                            <Typography>{`${product.name} x ${product.quantity}`}</Typography>
+                            <Typography>{`$${product.price}`}</Typography>
+                        </Box>
+                        {index < products.length - 1 && <Divider />}
+                    </Box>
+                ))}
             </Box>
             <Box sx={{ backgroundColor: "rgba(0, 0, 0, 0.02)", padding: "16px 48px" }}>
                 <Box sx={{ display: "flex", justifyContent: "space-between", fontWeight: "bold" }}>
