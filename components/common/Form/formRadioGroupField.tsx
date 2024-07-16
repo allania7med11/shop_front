64a806cb60fee0deb17c64169ee34124edb6da30
allania7med11 <allania7med11@gmail.com>
2024-07-16 17:26:58 +0000

    SHP-2406301__add FormRadioGroupField

---
+++ components/common/Form/formRadioGroupField.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Controller } from "react-hook-form";
+import { RadioGroup  } from "@mui/material";
+
+const FormRadioGroupField = ({
+    name,
+    control,
+    defaultValue = "",
+    rules,
+    children,
+    ...props
+}) => {
+    return (
+        <Controller
+            name={name}
+            control={control}
+            defaultValue={defaultValue}
+            rules={rules}
+            render={({ field }) => (
+                <RadioGroup {...field} {...props}>
+                    {children}
+                </RadioGroup>
+            )}
+        />
+    );
+};
+
+export default FormRadioGroupField;
