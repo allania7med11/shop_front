9af360c89d5a3ecefc24a0b81d2d65975d6dc995
allania7med11 <allania7med11@gmail.com>
2024-07-20 11:29:30 +0000

    SP-2463__display total_amount

--- components/pages/payments/CheckoutForm.tsx
+++ components/pages/payments/CheckoutForm.tsx
@@ -1,29 +1,50 @@
 import React from "react";
-import {  Radio, FormControlLabel, FormControl, Typography, Box } from "@mui/material";
+import {
+  Radio,
+  FormControlLabel,
+  FormControl,
+  Typography,
+  Box,
+} from "@mui/material";
 import CreditCardDetail from "@/components/common/creditCardDetail";
 import { UseFormReturn } from "react-hook-form";
 import FormRadioGroupField from "@/components/common/Form/formRadioGroupField";
 import { IsOrder } from "@/data/cart";
+import { useCurrentCartQuery } from "@/store/reducer/apis/cartApi";
 
-const CheckoutForm: React.FC<{ form: UseFormReturn<IsOrder>  }> = ({ form }) => {
-  const { control, watch } = form
+const CheckoutForm: React.FC<{ form: UseFormReturn<IsOrder> }> = ({ form }) => {
+  const { control, watch } = form;
   const paymentMethod = watch("payment.payment_method");
+  const { data } = useCurrentCartQuery();
+  const total_amount = data ? data.total_amount : "";
 
   return (
     <>
       <FormControl component="fieldset">
-        <FormRadioGroupField name="payment.payment_method" control={control} rules={{ required: "Payment Method is required" }}>
-          <FormControlLabel value="stripe" control={<Radio />} label="Credit Card" />
+        <FormRadioGroupField
+          name="payment.payment_method"
+          control={control}
+          rules={{ required: "Payment Method is required" }}
+        >
+          <FormControlLabel
+            value="stripe"
+            control={<Radio />}
+            label="Credit Card"
+          />
           {paymentMethod === "stripe" && (
             <Box sx={{ my: 2 }}>
               <CreditCardDetail />
             </Box>
           )}
-          <FormControlLabel value="cash_on_delivery" control={<Radio />} label="Pay with cash upon delivery" />
+          <FormControlLabel
+            value="cash_on_delivery"
+            control={<Radio />}
+            label="Pay with cash upon delivery"
+          />
         </FormRadioGroupField>
       </FormControl>
       <Typography variant="h6" gutterBottom>
-        All Total: $4551.62
+        All Total: ${total_amount}
       </Typography>
     </>
   );
