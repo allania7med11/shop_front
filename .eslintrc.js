7fb3a809ba1bb5eb5035131975422a3951099bab
allania7med11 <allania7med11@gmail.com>
2024-10-19 12:02:49 +0000

    SHP-2410111__add precommit for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true, // for client-side code
+    node: true, // for Node.js code (e.g., next.config.js)
+  },
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'eslint:recommended', // Use the recommended rules from ESLint
+    'plugin:@typescript-eslint/recommended', // Use the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays Prettier errors as ESLint errors
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    // Place your ESLint rules here
+  },
+};
