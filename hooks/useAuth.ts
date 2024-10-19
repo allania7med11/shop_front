f89db08bd2f9a33e2de1f2f7eb1780702f708b00
allania7med11 <allania7med11@gmail.com>
2024-10-19 15:25:18 +0000

    SHP-2410111__use const on error only

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -2,12 +2,15 @@ import { useGetUserProfileQuery } from '@/store/reducer/apis/authApi';
 import { getFullName } from '@/utils/auth';
 
 const useAuth = () => {
-  const { data: profile = false, error } = useGetUserProfileQuery();
+  let { data: profile = false } = useGetUserProfileQuery();
+  const { error } = useGetUserProfileQuery();
   if (error) {
     profile = false;
   }
+
   const isAuthenticated = !!profile;
   const fullName = profile ? getFullName(profile) : null;
+
   return { isAuthenticated, profile, fullName };
 };
 
