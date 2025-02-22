54e56d5b95933641d61b9b5ad527416b59716887
allania7med11 <allania7med11@gmail.com>
2025-02-22 14:56:04 +0000

    8697rwhk6__add admin chat page

--- pages/dashboard/chat/index.tsx
+++ pages/dashboard/chat/index.tsx
@@ -2,23 +2,18 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Copyright from 'src/Copyright';
 import Box from '@mui/material/Box';
-import useRedirectIfAnonymous from '@/hooks/useRedirectIfAnonymous';
-import { UpdateAccount } from '@/components/pages/profile/updateAccount';
 import { CustomBreadcrumbs, IsUrl } from '@/components/common/customBreadcrumb';
 
 export default function Index() {
-  useRedirectIfAnonymous();
   const urls: IsUrl[] = [
     { name: 'Home', href: `/` },
-    { name: 'Profile', href: `/auth/profile/` },
+    { name: 'Chat', href: `/dashboard/chat/` },
   ];
   return (
     <Container maxWidth={false} sx={{ maxWidth: '1400px' }}>
       <CustomBreadcrumbs sx={{ pt: 2 }} urls={urls} />
       <Box sx={{ my: 2 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center', pt: 4 }}>
-          <UpdateAccount />
-        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center', pt: 7 }}></Box>
         <Copyright sx={{ py: 7 }} />
       </Box>
     </Container>
