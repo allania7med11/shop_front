6dfb4eedce9694c143b0d7a867b32e1b0b6dce19
allania7med11 <allania7med11@gmail.com>
2025-03-15 16:16:47 +0000

    update ssg output to avoid docker map issues on regenerate folders

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isSSGBuild = process.env.BUILD_TARGET === 'ssg';
 
 module.exports = {
   output: isSSGBuild ? 'export' : undefined, // Enables static export
-  distDir: isSSGBuild ? 'shop_front' : '.next', // Use a custom folder for SSG
+  distDir: isSSGBuild ? 'shop_front/out' : '.next', // Use a custom folder for SSG
   typescript: {
     ignoreBuildErrors: true,
   },
